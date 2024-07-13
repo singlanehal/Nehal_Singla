35134c41f23d6aa4acf6b9927509c65d5d1f4201
Singla.nehal <nsingla3_be22@thapar.edu>
2024-07-13 10:26:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Full Stack Developer","UI/UX Designer","Frontend Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
